88e5978abc524984e36a2470a411d804702db7d0
valdemarnanon <139665106+valdemarnanon@users.noreply.github.com>
2023-08-08 16:19:33 -0300

    att

---
+++ aula003/index.js
@@ -0,0 +1,5 @@
+console.log('Valdemar "Nanon"'); // String = texto
+console.log("Valdemar 'Nanon'"); // Number = Número
+console.log(`Valdemar Nanon`);
+
+console.log(32, 15.85, 'Valdemar Nanon'); //
